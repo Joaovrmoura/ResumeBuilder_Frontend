12ba6d8a3b1548e4a143b06f7a286b1d255e4d06
joaovrmoura <joaovrmoura4@gmail.com>
2025-06-25 01:55:16 -0300

    show not

--- src/utils/ErrorHandler.js
+++ src/utils/ErrorHandler.js
@@ -4,7 +4,7 @@ export function handleApiError(responseData) {
     if (!responseData.success) {
         if (Array.isArray(responseData.message)) {
             const messages = responseData.message.map(e => `• ${e.msg}`).join('\n');
-            showNotification("Erros de validação:\n" + messages + 'em' + type + ' ' + value );
+            showNotification("Erros de validação:\n" + messages );
         } else {
             showNotification(responseData.message);
         }
