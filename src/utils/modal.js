f79e100e691c000ac58ed39e283a33b934d33897
joaovrmoura <joaovrmoura4@gmail.com>
2025-06-25 05:20:55 -0300

    btn login

--- src/utils/modal.js
+++ src/utils/modal.js
@@ -182,7 +182,6 @@ class ModalLogin {
         document.querySelectorAll('a[href^="#"]').forEach(anchor => {
             anchor.addEventListener('click', function (e) {
                 e.preventDefault();
-                const target = document.querySelector(this.getAttribute('href'));
                 if (target) {
                     target.scrollIntoView({
                         behavior: 'smooth',
